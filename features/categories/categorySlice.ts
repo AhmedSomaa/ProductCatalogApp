196ca1870bc9d1cebee527ecd5282d8c5b870113
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2021-08-14 19:03:19 +0200

    feat: Create categories slice

---
+++ features/categories/categorySlice.ts
@@ -0,0 +1,51 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState, AppThunk } from "../../store/store";
+
+export interface CategoriesState {
+  categories: string[];
+  status: "idle" | "loading" | "failed";
+}
+
+const initialState: CategoriesState = {
+  categories: [],
+  status: "idle",
+};
+
+export const fetchCategoriesAsync: any = createAsyncThunk(
+  "counter/fetchCategories",
+  async () => {
+    try {
+      const response = await fetch(
+        "https://fakestoreapi.com/products/categories"
+      );
+      return await response.json();
+    } catch (error) {
+      Promise.reject(error);
+    }
+  }
+);
+
+export const categorySlice = createSlice({
+  name: "categories",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchCategoriesAsync.fulfilled]: (state, action) => {
+      state.categories = action.payload;
+      state.status = "idle";
+    },
+    [fetchCategoriesAsync.pending]: (state, action) => {
+      state.categories = [];
+      state.status = "loading";
+    },
+    [fetchCategoriesAsync.rejected]: (state, action) => {
+      state.categories = [];
+      state.status = "failed";
+    },
+  },
+});
+
+export const selectCategories = (state: RootState) =>
+  state.categories.categories;
+
+export default categorySlice.reducer;
