b8b2c622a19333a1beb60e1cae464e5ba2d0dddf
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2021-08-14 23:47:18 +0200

    feat: Add selectable chips

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,12 +1,15 @@
+import { Box, HStack, ScrollView, Spinner } from "native-base";
 import * as React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text } from "react-native";
+import SelectableChips from 'react-native-chip/SelectableChips';
 
-import { Text, View } from "../components/Themed";
+import { View } from "../components/Themed";
 import { fetchCategoriesAsync, selectCategories, selectCategoriesStatus } from "../features/categories/categorySlice";
 import { useAppDispatch, useAppSelector } from "../store/store.hooks";
 
 export default function TabTwoScreen() {
   const dispatch = useAppDispatch();
+  const [filter, setFilter] = React.useState(["all"]);
   const categories = useAppSelector(selectCategories);
   const isLoading = useAppSelector(selectCategoriesStatus);
 
@@ -19,22 +22,60 @@ export default function TabTwoScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Catalogue</Text>
-      {isLoading === 'loading' ? "Loading Categories" : categories.map((el, i) => (
-        <Text key={i}>{el}</Text>
-      ))}
-    </View>
+      {isLoading === "loading" ? (
+        <Spinner size="sm" accessibilityLabel="Loading Categories" />
+      ) : (
+        <View style={{ height: 50, backgroundColor: "smokewhite" }}>
+          <ScrollView horizontal={true}>
+            <SelectableChips
+              alertRequired={false}
+              initialChips={categories}
+              chipStyle={styles.badge}
+              valueStyle={styles.badgeValue}
+              valueStyleSelected={styles.badgeValueSelected}
+              onChangeChips={(chips: any) => setFilter([...chips])}
+            />
+          </ScrollView>
+        </View>
+      )
+      }
+      <View style={styles.products}>
+        <Text>
+          {filter.join("-")}
+        </Text>
+      </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 5,
+    backgroundColor: "smokewhite",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
+  badge: {
+    borderWidth: 0,
+    borderColor: 'none',
+    backgroundColor: "transparent"
   },
+  badgeValue: {
+    padding: 5,
+    fontSize: 15,
+    color: "black",
+    borderRadius: 10,
+    backgroundColor: "white"
+  },
+  badgeValueSelected: {
+    padding: 5,
+    fontSize: 15,
+    color: "white",
+    borderRadius: 10,
+    backgroundColor: "orange"
+  },
+  products: {
+    padding: 5,
+    flexGrow: 1,
+    backgroundColor: "whitesmoke",
+  }
 });
