f444b9dd728e527ed0378bec0a40aa51d9bfb102
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2021-08-14 19:15:54 +0200

    feat: Add loading state in categories page

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -2,15 +2,13 @@ import * as React from "react";
 import { StyleSheet } from "react-native";
 
 import { Text, View } from "../components/Themed";
-import {
-  fetchCategoriesAsync,
-  selectCategories,
-} from "../features/categories/categorySlice";
+import { fetchCategoriesAsync, selectCategories, selectCategoriesStatus } from "../features/categories/categorySlice";
 import { useAppDispatch, useAppSelector } from "../store/store.hooks";
 
 export default function TabTwoScreen() {
   const dispatch = useAppDispatch();
   const categories = useAppSelector(selectCategories);
+  const isLoading = useAppSelector(selectCategoriesStatus);
 
   React.useEffect(() => {
     const getCategoriesPromise = dispatch(fetchCategoriesAsync());
@@ -21,8 +19,8 @@ export default function TabTwoScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Catalog</Text>
-      {categories.map((el, i) => (
+      <Text style={styles.title}>Catalogue</Text>
+      {isLoading === 'loading' ? "Loading Categories" : categories.map((el, i) => (
         <Text key={i}>{el}</Text>
       ))}
     </View>
