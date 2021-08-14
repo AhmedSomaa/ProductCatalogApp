6e4945b6831625a6c2a632328a902d847cd56637
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2021-08-14 19:04:07 +0200

    feat: Retrieve categories from state

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,12 +1,30 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { StyleSheet } from "react-native";
 
-import { Text, View } from '../components/Themed';
+import { Text, View } from "../components/Themed";
+import {
+  fetchCategoriesAsync,
+  selectCategories,
+} from "../features/categories/categorySlice";
+import { useAppDispatch, useAppSelector } from "../store/store.hooks";
 
 export default function TabTwoScreen() {
+  const dispatch = useAppDispatch();
+  const categories = useAppSelector(selectCategories);
+
+  React.useEffect(() => {
+    const getCategoriesPromise = dispatch(fetchCategoriesAsync());
+    return () => {
+      getCategoriesPromise.abort();
+    };
+  }, [dispatch]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Catalog</Text>
+      {categories.map((el, i) => (
+        <Text key={i}>{el}</Text>
+      ))}
     </View>
   );
 }
@@ -14,11 +32,11 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-  }
+    fontWeight: "bold",
+  },
 });
