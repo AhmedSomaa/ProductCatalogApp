7de767ffd77d992c827077c5ad0f02d16e8d2123
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2021-08-15 11:24:40 +0200

    chore: Move categorySlice to store folder

--- store/categorySlice.ts
+++ store/categorySlice.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-import { RootState } from "../../store/store";
+import { RootState } from "./store";
 
 export interface CategoriesState {
   categories: string[];
